80ccc6254cb2c037b852658045be27cd631e0b7e
Hye Chang <hyechang@uchicago.edu>
2018-06-16 14:44:26 -0400

    tests added

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,3 +1,5 @@
+# services/users/project/__init__.py
+
 import os
 import sys
 import datetime
@@ -5,42 +7,53 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-
 # instantiate the database
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# set config
-#app.config.from_object('project.config.DevelopmentConfig')
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    app.shell_context_processor({'app': app, 'db': db})
+    return app
 
 
 # model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+# class User(db.Model):
+#     __tablename__ = "users"
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.String(128), nullable=False)
+#     email = db.Column(db.String(128), nullable=False)
+#     active = db.Column(db.Boolean(), default=True, nullable=False)
+#
+#     def __init__(self, username, email):
+#         self.username = username
+#         self.email = email
 
 
 #print(app.config, file=sys.stderr)
 
-@app.route('/')
-def sanity_check():
-    return "your app works! check http://localhost:5000/users/ping"
 
-
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+# @app.route('/')
+# def sanity_check():
+#     return "your app works! check http://localhost:5000/users/ping"
+#
+#
+# @app.route('/users/ping', methods=['GET'])
+# def ping_pong():
+#     return jsonify({
+#         'status': 'success',
+#         'message': 'pong!'
+#     })
