4156954ff0a5156fe3510ea066147ad9bc91b7a4
Hye Chang <hyechang@uchicago.edu>
2018-06-16 12:34:21 -0400

    hmm did not create pg table

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,37 @@
+import os
+import sys
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 # instantiate the app
 app = Flask(__name__)
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
 
+# instantiate the database
+db = SQLAlchemy(app)
+
+# set config
+#app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+#print(app.config, file=sys.stderr)
 
 @app.route('/')
 def sanity_check():
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,18 +1,29 @@
+import os
+
 class BaseConfig:
-    """Base configuration"""
+    '''
+    Base configuration
+    '''
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
-    """Development configuration"""
-    pass
-
+    '''
+    Development configuration
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestingConfig(BaseConfig):
-    """Testing configuration"""
+    '''
+    Testing configuration
+    '''
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
-    """Production configuration"""
-    pass
+    '''
+    Production configuration
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
